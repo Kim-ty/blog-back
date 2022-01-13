a8070c62cf3373d1aca85e6823a6a47c2261fc1d
kty <tykim@ssrinc.co.kr>
2022-01-13 12:48:41 +0900

    Spring Boot 개발환경 세팅

---
+++ api/src/test/java/com/gabekim/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabekim.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
